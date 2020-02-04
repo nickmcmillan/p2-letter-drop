eee441bee6e969fae06ce4d13ce2c3d283fe6787
Nick McMillan <nick.a.mcmillan@gmail.com>
2020-02-04 20:01:14 +1100

    optos

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React, { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import p2 from 'p2'
-import useDimensions from './useDimensions'
 import { useWindowWidth } from '@react-hook/window-size'
 import delay from 'delay'
 import style from './styles.module.css'
 
 import characters from './characters'
 
-function mapRange (value, low1, high1, low2, high2) {
-  return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
 
-const randomChoice = arr => {
-  const randIndex = Math.floor(Math.random() * arr.length);
-  return arr[randIndex];
-};
-
-const contextScale = 0.133
+const scaleCharacter = 0.133
+const scaleContextSm = -200
+const scaleContextLg = -100
 
 const randomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const ssrWidth = 800
 
-function App() {
+function LetterDrop() {
   
-  const [ref, { width }] = useDimensions()
   const canvasRef = useRef()
   const rafRef = useRef()
 
@@ -39,24 +31,65 @@ function App() {
   const worldRef = useRef()
   const genericMaterialRef = useRef()
 
+  // Convert a canvas coordiante to physics coordinate
+  function getPhysicsCoord(e, canvas, w2, h2, scaleContextSize) {
+    const rect = canvas.getBoundingClientRect()
+    let x = (e.clientX - rect.left) * DPR
+    let y = (e.clientY - rect.top) * DPR
+
+    x = (x - w2 / 2) / scaleContextSize
+    y = (y - h2 / 2) / scaleContextSize
+
+    return [x, y]
+  }
+
+
+  function drawPlane(planeBody, ctx, w2) {
+    const y = planeBody.position[1]
+    ctx.moveTo(-w2, y)
+    ctx.lineTo(w2, y)
+  }
+
+
   // world and character set up, runs only once.
   useEffect(() => {
     worldRef.current = new p2.World({
-      // gravity: [0, -9.82],
-      gravity: [0, -10],
+      gravity: [0, -9.82],
       islandSplit: true,
-      sleepMode: p2.World.BODY_SLEEPING,
       // Defaults to SAPBroadphase
     })
+    
+
+    // https://github.com/schteppe/p2.js/issues/251
+    worldRef.current.sleepMode = p2.World.BODY_SLEEPING
+    worldRef.current.solver.iterations = 0.01
+    worldRef.current.doProfiling = true;
+    
+
 
     genericMaterialRef.current = new p2.Material()
+    // genericMaterialRef.current.stiffness = 1500;
+    // genericMaterialRef.current.relaxation = 3;
+    // genericMaterialRef.current.frictionStiffness = 1e8;
+    // genericMaterialRef.current.frictionRelaxation = 99999;
+    genericMaterialRef.current.friction = 0
+
+    console.log(genericMaterialRef.current)
+    
+
     worldRef.current.addContactMaterial(
-      new p2.ContactMaterial(genericMaterialRef.current, genericMaterialRef.current, { stiffness: 500 })
+      new p2.ContactMaterial(genericMaterialRef.current, genericMaterialRef.current, { restitution: 0.1, stiffness: 11500 })
     )
+
+    worldRef.current.defaultMaterial = genericMaterialRef.current
 
     async function getCharacters() {
       for (let c of characters) {
         c.shape.material = genericMaterialRef.current
+        
+        c.body.allowSleep = true
+        c.body.sleepSpeedLimit = 1; // Body will feel sleepy if speed < 1
+        c.body.sleepTimeLimit = 1; // Body falls asleep after being sleepy for 1s
         c.body.addShape(c.shape)
         worldRef.current.addBody(c.body)
         await delay(200)
@@ -64,30 +97,38 @@ function App() {
     }
     getCharacters();
 
+    return () => {
+      // TODO: maybe world clear?
+      worldRef.current.clear()
+    }
+
   }, [])
 
   useEffect(() => {
     if (typeof window === 'undefined') return
 
+    // need a wake up on resize, cos floor plane will have moved
+    characters.forEach(c => c.body.wakeUp())
+
+    const scaleContextSize = windowWidth <= 768 ? scaleContextLg : scaleContextSm
+
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
 
-    const w2 = windowWidth * DPR;
-    const h2 = (window.innerHeight * 0.75) * DPR;
-    canvas.width = w2;
-    canvas.height = h2;
+    const w2 = windowWidth * DPR
+    const h2 = (window.innerHeight * 0.75) * DPR
+    canvas.width = w2
+    canvas.height = h2
 
     let planeBody
     let mouseConstraint
 
-    const scale = -200
-
     worldRef.current.solver.tolerance = 0.001
-    worldRef.current.solver.iterations = 1;
-    // worldRef.current.solver.arrayStep = 1;
+    worldRef.current.solver.iterations = 4
+    // worldRef.current.solver.arrayStep = 1
     
-    ctx.lineWidth = 0.03;
-    ctx.font = '10px Druk';
+    ctx.lineWidth = 0.03
+    ctx.font = '10px Druk'
     
     // Add a plane
     const planeShape = new p2.Plane()
@@ -95,7 +136,7 @@ function App() {
     planeShape.material = genericMaterialRef.current
     // console.log(planeShape.material)
     planeBody = new p2.Body({
-      position: [0, (h2 / 2 - 50) / scale],
+      position: [0, (h2 * 0.5 - 50) / scaleContextSize],
       allowSleep: true,
     })
     planeBody.addShape(planeShape)
@@ -118,7 +159,7 @@ function App() {
         boxBody.velocity[0] = randomInRange(-2, 2)
         boxBody.velocity[1] = randomInRange(0, 2)
 
-        boxBody.angularVelocity = randomInRange(-2, 2)
+        boxBody.angularVelocity = randomInRange(-5, 5)
         
       }
 
@@ -128,38 +169,20 @@ function App() {
       ctx.rotate(boxBody.angle) // Rotate to the box body frame
 
       // render hit box
-      // ctx.rect(
-      //   -boxShape.width / 2,
-      //   -boxShape.height / 2,
-      //   boxShape.width,
-      //   boxShape.height,
-      // )
+      ctx.rect(
+        -boxShape.width / 2,
+        -boxShape.height / 2,
+        boxShape.width,
+        boxShape.height,
+      )
 
       ctx.fillStyle = '#2c2c2c'
 
-      ctx.scale(contextScale, contextScale)
-      // ctx.rotate(Math.PI)
+      ctx.scale(scaleCharacter, scaleCharacter)
+      ctx.rotate(Math.PI)
       // ctx.stroke()
       ctx.fillText(char, x, y)
       ctx.restore()
-    }
-
-    function drawPlane(planeBody, ctx) {
-      const y = planeBody.position[1]
-      ctx.moveTo(-w2, y)
-      ctx.lineTo(w2, y)
-    }
-
-    // Convert a canvas coordiante to physics coordinate
-    function getPhysicsCoord(e, canvas) {
-      const rect = canvas.getBoundingClientRect()
-      let x = (e.clientX - rect.left) * DPR
-      let y = (e.clientY - rect.top) * DPR
-
-      x = (x - w2 / 2) / scale
-      y = (y - h2 / 2) / scale
-
-      return [x, y]
     }
 
 
@@ -169,7 +192,7 @@ function App() {
       if (mouseButton && (mouseButton !== 0 && mouseButton !== 1)) return
 
       // Convert the canvas coordinate to physics coordinates
-      const position = getPhysicsCoord(e, canvas)
+      const position = getPhysicsCoord(e, canvas, w2, h2, scaleContextSize)
 
       // Check if the cursor is inside the box
       const hitBodies = worldRef.current.hitTest(position, characters.map(c => c.body))
@@ -190,7 +213,7 @@ function App() {
     }
 
     const onMove = function (e) {
-      const position = getPhysicsCoord(e.touches ? e.touches[0] : e, canvas)
+      const position = getPhysicsCoord(e.touches ? e.touches[0] : e, canvas, w2, h2, scaleContextSize)
       mouseBody.position[0] = position[0]
       mouseBody.position[1] = position[1]
     }
@@ -213,16 +236,13 @@ function App() {
       // Transform the canvas
       ctx.save()
       ctx.translate(w2 * 0.5, h2 * 0.5) // Translate to the center
-      ctx.scale(scale, scale)
+      ctx.scale(scaleContextSize, scaleContextSize)
 
       // Draw all bodies
       ctx.beginPath()
       
-      characters.forEach(({ character, body, shape, x, y}) => {
-        drawbox(character, body, shape, x, y)
-      })
-
-      drawPlane(planeBody, ctx)
+      characters.forEach(({ character, body, shape, x, y}) => drawbox(character, body, shape, x, y))
+      drawPlane(planeBody, ctx, w2)
 
       ctx.stroke()
 
@@ -250,7 +270,7 @@ function App() {
   }, [windowWidth])
 
   return (
-    <section className={style.section} ref={ref}>
+    <section className={style.section}>
       <canvas
         ref={canvasRef}
         className={style.canvas}
@@ -260,4 +280,4 @@ function App() {
 }
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<LetterDrop />, rootElement)
